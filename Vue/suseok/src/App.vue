18f6c97c8b6e2ab78f078b51f2395d601740f7c1
Seokmin Yoo <qusgh1027@naver.com>
2024-05-14 13:16:02 +0900

    0514Vue

--- Vue/suseok/src/App.vue
+++ Vue/suseok/src/App.vue
@@ -1,8 +1,20 @@
-<script setup>
-
-</script>
 <template>
-
-</template>
-<style scoped>
-</style>
+    <div id="app">
+      <TheHamburgerButton />
+    </div>
+  </template>
+  
+  <script setup>
+  import TheHamburgerButton from './components/common/TheHamburgerButton.vue';
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  </style>
+  
